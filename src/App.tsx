cb1fe874bb3183baf2d0d1e14ff43813291a28da
Paul DeVito <pdevito3@gmail.com>
2023-11-02 17:39:36 -0400

    build: handling for .net

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,77 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
 function App() {
+  const environment = getEnv();
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="p-8">
+      <h1 className="text-xl font-bold text-violet-500">
+        Hello React Dash in ({environment})
+      </h1>
+      <Recipes />
+    </div>
+  );
+}
+
+function useRecipes() {
+  return useQuery({
+    queryKey: ["recipes"],
+    queryFn: async () =>
+      axios
+        .get(
+          getEnv() === "Standalone"
+            ? "https://localhost:5375/api/recipes"
+            : "/api/recipes"
+        )
+        .then((response) => response.data),
+  });
+}
+
+export function getEnv() {
+  const env = window.ASPNETCORE_ENVIRONMENT;
+  return env === "{{ASPNETCORE_ENVIRONMENT}}" ? "Standalone" : env;
+}
+
+// The component that displays the recipes in a card format
+function Recipes() {
+  const { isLoading, data } = useRecipes();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+      {data &&
+        data?.map(
+          (recipe: {
+            id: string;
+            imageLink: string;
+            title: string;
+            description: string;
+            recipeSourceLink: string;
+          }) => (
+            <div key={recipe.id} className="p-4 bg-white rounded-lg shadow-lg">
+              <img
+                src={recipe.imageLink}
+                alt={recipe.title}
+                className="object-cover w-full h-32 sm:h-48"
+              />
+              <div className="p-4">
+                <h2 className="text-lg font-bold">{recipe.title}</h2>
+                <p className="text-sm">{recipe.description}</p>
+                <a
+                  href={recipe.recipeSourceLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Source
+                </a>
+              </div>
+            </div>
+          )
+        )}
+    </div>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App, { getEnv } from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+
+const env = getEnv();
+const ReactQueryDevtools =
+  env === "Production"
+    ? () => null // Render nothing in production
+    : React.lazy(() =>
+        // Lazy load in development
+        import("@tanstack/react-query-devtools").then((res) => ({
+          default: res.ReactQueryDevtools,
+          // For Embedded Mode
+          // default: res.TanStackRouterDevtoolsPanel
+        }))
+      );
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
